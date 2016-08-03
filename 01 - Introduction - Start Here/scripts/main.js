8c86ff7e30bd4884880241349322023eb21801ed
Christian Acuna <cacuna0828@gmail.com>
2016-08-03 16:52:19 +0800

    Add event listener

--- 01 - Introduction - Start Here/scripts/main.js
+++ 01 - Introduction - Start Here/scripts/main.js
@@ -5,6 +5,7 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var h = require('./helpers');
@@ -84,11 +85,19 @@ var Inventory = React.createClass({
 */
 
 var StorePicker = React.createClass({
+  mixins : [History],
+  goToStore : function (event) {
+    event.preventDefault();
+    //get the data frm the input
+    var storeId = this.refs.storeId.value;
+    // transition from <StorePicker/> to <App/>
+    this.history.pushState(null, '/store/' + storeId);
+  },
 
   render : function() {
     var name = "Chirstian";
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         {/* comment goes in here! */}
         <h2>Please Enter A Store</h2>
         <input type="text" ref="storeId" defaultValue= {h.getFunName()} required/>
